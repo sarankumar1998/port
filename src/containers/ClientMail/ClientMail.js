29ed1d9a5c89541c7a948aca10c3c9533a7b74a4
saran kumar <lmsdev2@nipponpaint.co.in>
2023-07-13 17:53:44 +0530

    timer set in otp

--- src/containers/ClientMail/ClientMail.js
+++ src/containers/ClientMail/ClientMail.js
@@ -6,11 +6,13 @@ function ClientMail() {
   const [email, setEmail] = useState('');
   const [otp, setOTP] = useState('');
   const [message, setMessage] = useState('');
-
+  const [messageStyle, setMessageStyle] = useState({});
 
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const [count, setCount] = useState(0); // Start count from 0
 
   useEffect(() => {
     fetchData();
@@ -18,16 +20,18 @@ function ClientMail() {
 
   const fetchData = async () => {
     try {
-    const response = await axios.get('https://api.example.com/data');
+      const response = await axios.get('https://api.example.com/data');
       setData(response.data);
       setLoading(false);
     } catch (error) {
-      setError(error.message);
+      // setError(error.message);
       setLoading(false);
     }
   };
 
   const handleGenerateOTP = () => {
+    setMessage('Generating OTP...');
+    setCount(5 * 60); // Set count to 5 minutes (300 seconds)
     axios
       .post('http://localhost:4000/api/v6/generate-otp', { email })
       .then(response => {
@@ -36,53 +40,71 @@ function ClientMail() {
       .catch(error => {
         console.error(error);
         setMessage('Failed to generate OTP.');
+        setCount(0); // Reset count to 0
       });
   };
+
+  useEffect(() => {
+    if (count > 0) {
+      const timer = setTimeout(() => {
+        setCount(prevCount => prevCount - 1);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [count]);
 
   const handleVerifyOTP = () => {
     axios
       .post('http://localhost:4000/api/v6/verify-otp', { email, otp })
       .then(response => {
-        console.log(response.status)
+        console.log(response.status);
         setMessage(response.data.message);
       })
       .catch(error => {
         console.error(error.response.data.message);
         setMessage(error.response.data.message);
+        if (error.response.status === 400 || error.response.status === 500) {
+          setMessageStyle({ color: 'red' });
+        }
       });
   };
 
   return (
-    <div>
+    <div style={{ margin: '3rem' }}>
       <h1>Login via OTP</h1>
+
       <input
         type="email"
         placeholder="Enter your email"
         value={email}
         onChange={e => setEmail(e.target.value)}
-      />
-      <button className='btn btn-primary btn-sm' onClick={handleGenerateOTP}>Generate OTP</button>
-      <br/>
+      />{' '}
+      &nbsp;
+      <button className="btn btn-primary btn-sm" onClick={handleGenerateOTP}>
+        Generate OTP
+      </button>
+      <br />
+      <p>
+        {Math.floor(count / 60)}:{(count % 60).toString().padStart(2, '0')}
+      </p>
 
-      <input
-        type="text"
-        placeholder="Enter OTP"
-        value={otp}
-        onChange={e => setOTP(e.target.value)}
-      />
-      <button className='btn btn-success btn-sm' onClick={handleVerifyOTP}>Verify OTP</button>
+      <div className="mt-3">
+        <input
+          type="text"
+          placeholder="Enter OTP"
+          value={otp}
+          onChange={e => setOTP(e.target.value)}
+        />{' '}
+        &nbsp;
+        <button className="btn btn-success btn-sm" onClick={handleVerifyOTP}>
+          Verify OTP
+        </button>
+      </div>
 
-      <p>{message}</p>
-
-      <div>
-      {loading ? (
-        <Skeleton variant="rect" width={300} height={200} />
-      ) : error ? (
-        <div>Error: {error}</div>
-      ) : (
-        <div>{/* Render your fetched data here */}</div>
-      )}
-    </div>
+      <p style={messageStyle}>{message}</p>
     </div>
   );
 }
