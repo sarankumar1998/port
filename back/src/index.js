0f5447a3107c437a9c90d4e267127d53d7b34ee9
saran kumar <lmsdev2@nipponpaint.co.in>
2023-07-25 17:09:53 +0530

    ok

--- back/src/index.js
+++ back/src/index.js
@@ -7,7 +7,7 @@ const sports = require("./sportsApi");
 const mail = require("./Mailauth/Mail");
 const order = require("./Payment/Payment");
 const mailLogin = require("./MailLogin/MailLogin");
-// const image = require("./ImageUpload/Ig");
+const pass = require("./Passengers/Passengers");
 const cors = require("cors");
 
 var app = express();
@@ -21,7 +21,7 @@ app.use("/api/v3", sports);
 app.use("/api/v4", mail);
 app.use("/api/v5", order);
 app.use("/api/v6", mailLogin);
-// app.use("/api/v6", image);
+app.use("/api/v7", pass);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
