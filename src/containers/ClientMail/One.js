8421d622440b164c87f28dc2926b83d006245735
saran kumar <lmsdev2@nipponpaint.co.in>
2023-07-26 17:11:59 +0530

    ok

--- src/containers/ClientMail/One.js
+++ src/containers/ClientMail/One.js
@@ -1,154 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import Navbars from '../../components/Navbars'
-import axios from 'axios'
+import React, { useState } from 'react';
+import Navbars from '../../components/Navbars';
+import axios from 'axios';
 
 export default function One() {
-
-
-
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
-  const [persons, setPersons] = useState([])
-  const [pass, setPass] = useState([])
-  console.log(persons);
-
-  // const AddPersons = () => {
-  //   const Obj = { name, age: parseInt(age) }
-  //   if (Obj && persons.length < 6) {
-  //     setPersons([Obj, ...persons])
-  //     setAge('')
-  //     setName('')
-  //   }
-  // }
-
-
-  useEffect(() => {
-    PassengerDetails()
-  }, [])
-
-
-  const PassengerDetails = () => {
-    axios.get("http://localhost:4000/api/v7/persons")
-      .then((res) => {
-        setPass(res.data)
-      })
-  }
+  const [route, setRoute] = useState(JSON.parse(sessionStorage.getItem('user')) || {});
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [persons, setPersons] = useState([]);
 
   const AddPersons = () => {
-    let passer = {
-      name: name,
-      age: age
+    const Obj = { name, age: parseInt(age), userId: route.id };
+    if (Obj && persons.length < 6) {
+      setPersons([...persons, Obj]);
+      setAge('');
+      setName('');
     }
-    axios.post(`http://localhost:4000/api/v7/pass`, passer)
-      .then(res => {
-        if (res.status === 200) {
-          sessionStorage.setItem(
-            "Passengers",
-            JSON.stringify({
-              state: { passID: res.data }
-            })
-          );
-        }
+  };
 
+  const handleSave = () => {
+    const data = {
+      persons,
+    };
 
+    axios
+      .post('http://localhost:4000/api/v7/pass', data)
+      .then((response) => {
+        console.log('Data saved successfully:', response.data);
       })
-  }
-
-
-
-  // const removePersons = (ind) => {
-  //   const Updatepersons = [...persons]
-  //   Updatepersons.splice(ind, 1)
-  //   setPersons(Updatepersons)
-  // }
-
-  const RemovePass = (id) => {
-    axios.delete('http://localhost:4000/api/v7/persons/' + id)
-      .then((res) => {
-        console.log("done");
-        setPass(prev => prev.filter(pass => pass.id !== id))
-      })
-  }
-
-
-
-
-
-
-  const [count, setCount] = useState(0);
-  const handleIncrement = () => {
-    setCount(prevCount => prevCount + 1);
+      .catch((error) => {
+        console.error('Error saving data:', error);
+      });
   };
 
-  const handleDecrement = () => {
-    setCount(prevCount => prevCount - 1);
-  };
-
-  const handleReset = () => {
-    setCount(0);
-  };
-
-  const handleButtonClick = (action) => {
-    switch (action) {
-      case 'increment':
-        handleIncrement();
-        break;
-      case 'decrement':
-        handleDecrement();
-        break;
-      case 'reset':
-        handleReset();
-        break;
-      default:
-        break;
-    }
+  const removePersons = (id) => {
+    const updatedPersons = [...persons];
+    updatedPersons.splice(id, 1);
+    setPersons(updatedPersons);
   };
 
   return (
     <div>
-
-      <Navbars />   <div style={{ margin: '5rem' }}>
+      <Navbars />
+      <div style={{ margin: '5rem' }}>
         <h4 className='text-info'>Welcome</h4>
-        {/* <p>{ogis}</p> */}
-
         <div>
-          <p>Count: {count}</p>
-          <button onClick={() => handleButtonClick('increment')}>Increment</button>
-          <button onClick={() => handleButtonClick('decrement')}>Decrement</button>
-          <button onClick={() => handleButtonClick('reset')}>Reset</button>
-        </div>
-
-
-        <div>  <label>Name:</label> <input name='name' value={name} placeholder='name' onChange={(e) => setName(e.target.value)} /> <br />
-          <label>Age:</label>   <input name='age' value={age} className='mt-3' placeholder='age' onChange={(e) => setAge(e.target.value)} /> &nbsp;
-          <button onClick={AddPersons} className='btn btn-success'>Add</button>
-
-
-          {pass.length > 0 &&
+          <label>Name:</label>
+          <input name='name' value={name} placeholder='name' onChange={(e) => setName(e.target.value)} /> <br />
+          <label>Age:</label>
+          <input name='age' value={age} className='mt-3' placeholder='age' onChange={(e) => setAge(e.target.value)} />
+          &nbsp;
+          <button onClick={AddPersons} className='btn btn-success'>
+            Add
+          </button>
+          {persons.length > 0 && (
             <div className='mt-5'>
               <h4>Passengers</h4>
-              {pass.map((e) => {
-                return (
-                  <><ul>{e.name} &nbsp; {e.age}               &nbsp;
-
-                    <button onClick={() => RemovePass(e.id)} className='btn btn-sm btn-danger'>Remove</button></ul>
-                  </>
-
-                )
-
-
-              })}
-            </div>}
-
-
+              {persons.map((e, id) => (
+                <div key={id}>
+                  {e.name} &nbsp; {e.age} &nbsp;
+                  <button onClick={() => removePersons(id)} className='btn btn-sm btn-danger'>
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+          <div>
+            <button className='btn btn-warning btn-sm' onClick={handleSave}>
+              Save
+            </button>
+          </div>
         </div>
       </div>
-
-
-
-
-
     </div>
-
-  )
+  );
 }
